0e481cef0e51f83273baf26faf6c0a3e9d38de49
Syifa Aulia <16053440@BTPN>
2018-10-25 14:49:30 +0700

    installing redux-saga and devtools

--- src/store.js
+++ src/store.js
@@ -1,6 +1,18 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware  } from 'redux';
 import reducers from './reducers';
+import createSagaMiddleware from 'redux-saga';
+import composeWithDevTools from 'remote-redux-devtools';
 
+import { Sagas } from '../src/Midleware/Sagas'
 
-const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const sagaMiddleware = createSagaMiddleware()
+
+const store = createStore(
+  reducers, 
+  composeWithDevTools(
+    applyMiddleware(sagaMiddleware),
+  ));
+
+  sagaMiddleware.run(Sagas);
+  
 export default store; 
